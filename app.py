d7d4adec237e4f75e7997e2bf6e83f96ae4815e4
sebastianbk98 <92931168+sebastianbk98@users.noreply.github.com>
2023-01-31 22:12:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,18 +12,11 @@ sepalLengthCM = st.slider('Sepal Length', 0.0,10.0, 1.0)
 sepalWidthCM = st.slider('Sepal Width', 0.0,10.0, 1.0)
 petalLengthCM = st.slider('Petal Length', 0.0,10.0, 1.0)
 petalWidthCM = st.slider('Petal Width', 0.0,10.0, 1.0)
-data1 = {
-    'SepalLengthCm': sepalLengthCM,
-    'SepalWidthCm': sepalWidthCM,
-    'PetalLengthCm': petalLengthCM,
-    'PetalWidthCm': petalWidthCM,
-}
-feature_list = [sepalLengthCM,sepalWidthCM,petalLengthCM,petalWidthCM,]
-                
-single_sample = np.array(feature_list).reshape(1, -1)
 
 if st.button("Click to Predict"):    
-    loaded_model = pickle.load(open('model.sav', "rb"))
+    feature_list = [sepalLengthCM,sepalWidthCM,petalLengthCM,petalWidthCM,]            
+    single_sample = np.array(feature_list).reshape(1, -1)
+    loaded_model = pickle.load(open('model.pkl', "rb"))
     prediction = loaded_model.predict(single_sample)
     if (prediction[0]=="Iris-virginica"):
         st.image("virginica.png")
